76b6587829d07b50ef03b7401a752bae166d7a20
subrat.thakur <subrat.thakur@salesforce.com>
2020-04-15 12:11:32 +0530

    build and readme file

---
+++ client/build/precache-manifest.b5569a90186cefd887a670d30cc28d88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "645c3fc0ad6b017890344f39f6f16519",
+    "url": "/static/media/background.645c3fc0.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2dde8693e365670598a",
+    "url": "/static/js/main.c2dde869.chunk.js"
+  },
+  {
+    "revision": "8ad8d4978a21531a821c",
+    "url": "/static/js/2.8ad8d497.chunk.js"
+  },
+  {
+    "revision": "c2dde8693e365670598a",
+    "url": "/static/css/main.5422aefd.chunk.css"
+  },
+  {
+    "revision": "8ad8d4978a21531a821c",
+    "url": "/static/css/2.584d7901.chunk.css"
+  },
+  {
+    "revision": "d24632fdb04994c8348bb17604b45f4b",
+    "url": "/index.html"
+  }
+];
